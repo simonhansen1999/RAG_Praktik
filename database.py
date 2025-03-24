67be9c15ca84c4a97e7200aed204291452714bc3
Simon <seh500@hotmail.com>
2025-03-24 17:32:09 +0100

    Fiksede mere deprecated

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import chromadb
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 
 load_dotenv()
